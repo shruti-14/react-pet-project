b311797236a36d0d6d3082b6a6a8434d14f36448
ShrutiBadekar <shrtbadekar@gmail.com>
2019-04-14 23:32:58 +0530

    Initial structure and person component created and initialised used state via hooks

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const app = (props) =>{
+  const [personState, setPersonState]= useState({
+    persons:[
+      {name:"shruti",age:22},
+      {name:"karishma",age:19}
+    ]
+  }); //returns an array of two elements
+  //first having the current state itself
+  //second having a function responsible for updating the state
+  const [otherState, setOtherState]= useState('Some other value');
+  const switchNameHandler=()=>{
+    setPersonState({
+      persons:[
+        {name:"shruti",age:23},
+        {name:"karishma",age:20}
+      ]
+    })
   }
-}
-
-export default App;
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <p>This is really working!</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name = {personState.persons[0].name} 
+      age = {personState.persons[0].age}
+      />
+       <Person
+        name={personState.persons[1].name}
+        age={personState.persons[1].age}
+      >
+        My Hobbies: Racing
+      </Person>
+    </div>
+  );
+}  
+export default app;
